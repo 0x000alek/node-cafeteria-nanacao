c5430d4e12fb5eb2e4d46e9b0405192c23f8a47e
0x000alek <aolguin.or@gmail.com>
2025-07-07 16:39:39 -0400

    feat: versión 1.0.0

---
+++ tests/server.spec.js
@@ -0,0 +1,50 @@
+const request = require("supertest");
+const server = require("../index");
+
+describe("Operaciones CRUD de cafes", () => {
+  describe("GET /cafes", () => {
+    it("should return a 200 status and an array with at least one object", async () => {
+      const response = await request(server).get("/cafes");
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toBeInstanceOf(Array);
+      expect(response.body.length).toBeGreaterThanOrEqual(1);
+      expect(response.body[0]).toBeInstanceOf(Object);
+    });
+  });
+
+  describe("DELETE /cafes/:id", () => {
+    it("should return a 404 status if the cafe id does not exist", async () => {
+      const nonExistentId = 5;
+      const requestParamsId = nonExistentId;
+      const response = await request(server)
+        .delete(`/cafes/${requestParamsId}`)
+        .set("Authorization", "Bearer valid_token");
+
+      expect(response.statusCode).toBe(404);
+    });
+  });
+
+  describe("POST /cafes", () => {
+    it("should return a 201 status when a new cafe is added", async () => {
+      const payload = { id: 5, nombre: "Flat White" };
+      const response = await request(server)
+        .post("/cafes")
+        .send(payload);
+
+      expect(response.statusCode).toBe(201);
+    });
+  });
+
+  describe("PUT /cafes/:id", () => {
+    it("should return a 400 status code if the ID in the params is different from the ID in the payload", async () => {
+      const requestParamsId = 1;
+      const payload = { id: 6, nombre: "Espresso" };
+      const response = await request(server)
+        .put(`/cafes/${requestParamsId}`)
+        .send(payload);
+
+      expect(response.statusCode).toBe(400);
+    });
+  });
+});
