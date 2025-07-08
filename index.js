c5ad14de1dfb9629c591154d69bcff7de33bf723
0x000alek <aolguin.or@gmail.com>
2025-07-08 14:56:16 -0400

    chore: actualizar versiones de express, supertest y jest a las más recientes

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(json());
 
 app.use(serverRoutes);
-app.use('*', (req, res) => {
+app.use((req, res) => {
   res.status(404).send({ message: 'La ruta que intenta consultar no existe' });
 });
 
-const server = app.listen(3000, console.log('SERVER ON'));
+const server = app.listen(3000, () => {
+  console.log('SERVER ON');
+});
 
 export default server;
